3e4343105972d88f8a97642bd4736dcee582f2b9
Hitesh Kumar Sharma <hiteshupes@gmail.com>
2023-05-07 21:38:49 +0530

    failed test case

--- src/test/java/gfg/proj1/MyCalcTest.java
+++ src/test/java/gfg/proj1/MyCalcTest.java
@@ -10,7 +10,7 @@ public class MyCalcTest {
 	@Test
 	public void test() {		
 		MyCalc ob = new MyCalc();
-		assertEquals(30, ob.sum(20, 10));
+		assertEquals(20, ob.sum(20, 10));
 		assertEquals(10, ob.diff(20, 10));
 		assertEquals(200, ob.mul(20, 10));
 		assertEquals(2, ob.div(20, 10));
